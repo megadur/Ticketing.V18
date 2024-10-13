d9489c1a16582e61812bad325d672a88405c1fb3
Mega Dur <megadur@gmail.com>
2024-10-13 09:54:58 +0200

    startet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { Observable } from 'rxjs';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Ticketing.V18';
+
+  error$: Observable<string>= new Observable<string>();
+  isLoading$: Observable<boolean>= new Observable<boolean>();
+
+  //constructor(private store: Store<RootState>) {}
+
+  ngOnInit() {
+    //this.error$ = this.store.select(RootStoreSelectors.selectError);
+    //this.isLoading$ = this.store.select(RootStoreSelectors.selectIsLoading);
+    //this.store.dispatch(new UserStoreActions.LoadUsersAction());
+    //this.store.dispatch(new TicketStoreActions.LoadTicketsAction());
+  }
 }
